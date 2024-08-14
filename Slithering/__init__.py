a927486909b96261f780487d28b3de687437b41f
Cody Clark <codyjamesclark@gmail.com>
2024-08-14 11:36:15 -0500

    organized classes into modules

---
+++ Slithering/__init__.py
@@ -0,0 +1,5 @@
+from .bullfrog import Bullfrog
+from .copperhead import Copperhead
+from .newt import Newt
+from .ratsnake import RatSnake
+from .salamander import Salamander
---
+++ Walking/__init__.py
@@ -0,0 +1,5 @@
+from .alpaca import Alpaca
+from .cow import Cow
+from .donkey import Donkey
+from .llama import Llama
+from .sheep import Sheep
