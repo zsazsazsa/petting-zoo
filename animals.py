c175a10d92a156c497c523a08db6cb494b64f153
Cody Clark <codyjamesclark@gmail.com>
2024-08-14 11:17:38 -0500

    refactored classes

--- animals.py
+++ animals.py
@@ -3,197 +3,182 @@ from datetime import date
 
 class Llama:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 class Donkey:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 class Sheep:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 class Alpaca:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 class Cow:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.walking = True
 
 class Mallard:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 class Goldfish:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 class Catfish:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 class Swan:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 class Koi:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.swimming = True
 
 class Copperhead:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
 class RatSnake:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
 class Salamander:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
 class Bullfrog:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
 class Newt:
 
-    def __init__(self):
+    def __init__(self, name, species):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
+        self.name = name
+        self.species = species
         self.date_added = date.today()
         self.slithering = True
 
-miss_fuzz = Llama()
-miss_fuzz.name = "Miss Fuzz"
+miss_fuzz = Llama("Miss Fuzz", "Gorgeous")
 
-hector = Donkey()
-hector.name = "Hector"
+hector = Donkey("Hector", "Handsome")
 
-ilana = Sheep()
-ilana.name = "Ilana"
+ilana = Sheep("Ilana", "Talkative")
 
-percy = Alpaca()
-percy.name = "Percy"
+percy = Alpaca("Percy", "Merciful")
 
-betty = Cow()
-betty.name = "Betty"
+betty = Cow("Betty", "Butt-kisser")
 
-dirk = Mallard()
-dirk.name = "Dirk"
+dirk = Mallard("Dirk", "Jerk")
 
-joyce = Goldfish()
-joyce.name = "Joyce"
+joyce = Goldfish("Joyce", "Wise")
 
-spencer = Catfish()
-spencer.name = "Spencer"
+spencer = Catfish("Spencer", "Thrifty")
 
-yvonne = Swan()
-yvonne.name = "Yvonne"
+yvonne = Swan("Yvonne", "Sensual")
 
-joy = Koi()
-joy.name = "Joy"
+joy = Koi("Joy", "Stupid")
 
-colin = Copperhead()
-colin.name = "Colin"
+colin = Copperhead("Colin", "Creative")
 
-rachel = RatSnake()
-rachel.name = "Rachel"
+rachel = RatSnake("Rachel", "Untrustworthy")
 
-sally = Salamander()
-sally.name = "Sally"
+sally = Salamander("Sally", "Salty")
 
-buford = Bullfrog()
-buford.name = "Buford"
+buford = Bullfrog("Buford", "Big Hearted")
 
-nell = Newt()
-nell.name = "Nell"
+nell = Newt("Nell", "Nervous")
 
-print(colin.name)
+print(colin.species)
