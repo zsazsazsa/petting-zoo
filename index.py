055eb73c966b41f46cbea5a5372a6d67355110ed
Cody Clark <codyjamesclark@gmail.com>
2024-08-14 13:23:53 -0500

    added feed method

--- index.py
+++ index.py
@@ -2,34 +2,34 @@ from Slithering import Copperhead, RatSnake, Salamander, Bullfrog, Newt
 from Swimming import Mallard, Goldfish, Catfish, Koi, Swan
 from Walking import Llama, Donkey, Sheep, Alpaca, Cow
 
-miss_fuzz = Llama("Miss Fuzz", "Llama", "Morning")
+miss_fuzz = Llama("Miss Fuzz", "Llama", "Morning", "Hay")
 
-hector = Donkey("Hector", "Donkey", "Midday")
+hector = Donkey("Hector", "Donkey", "Midday", "Beans")
 
-ilana = Sheep("Ilana", "Sheep", "Midday")
+ilana = Sheep("Ilana", "Sheep", "Midday", "Milk")
 
-percy = Alpaca("Percy", "Alpaca", "Afternoon")
+percy = Alpaca("Percy", "Alpaca", "Afternoon", "Corn on the Cob")
 
-betty = Cow("Betty", "Cow", "Afternoon")
+betty = Cow("Betty", "Cow", "Afternoon", "Cheese")
 
-dirk = Mallard("Dirk", "Mallard")
+dirk = Mallard("Dirk", "Mallard", "Breadcrumbs")
 
-joyce = Goldfish("Joyce", "Goldfish")
+joyce = Goldfish("Joyce", "Goldfish", "Seeds")
 
-spencer = Catfish("Spencer", "Catfish")
+spencer = Catfish("Spencer", "Catfish", "Mousefish")
 
-yvonne = Swan("Yvonne", "Swan")
+yvonne = Swan("Yvonne", "Swan", "Caviar")
 
-joy = Koi("Joy", "Koi")
+joy = Koi("Joy", "Koi", "Pebbles")
 
-colin = Copperhead("Colin", "Copperhead")
+colin = Copperhead("Colin", "Copperhead", "Smoked Salmon")
 
-rachel = RatSnake("Rachel", "Rat Snake")
+rachel = RatSnake("Rachel", "Rat Snake", "Escargot")
 
-sally = Salamander("Sally", "Salamander")
+sally = Salamander("Sally", "Salamander", "Flies")
 
-buford = Bullfrog("Buford", "Bullfrog")
+buford = Bullfrog("Buford", "Bullfrog", "Honey")
 
-nell = Newt("Nell", "Newt")
+nell = Newt("Nell", "Newt", "Ping Pong Balls")
 
-print(f'{hector.name} the {hector.species} is available to pet during the {hector.shift} shift.')
+print(sally)
