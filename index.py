edcb119a442ce57da14dac74aec1524d2e052429
Cody Clark <codyjamesclark@gmail.com>
2024-08-14 11:58:25 -0500

    added shift to walking acritters

--- index.py
+++ index.py
@@ -2,34 +2,34 @@ from Slithering import Copperhead, RatSnake, Salamander, Bullfrog, Newt
 from Swimming import Mallard, Goldfish, Catfish, Koi, Swan
 from Walking import Llama, Donkey, Sheep, Alpaca, Cow
 
-miss_fuzz = Llama("Miss Fuzz", "Gorgeous")
+miss_fuzz = Llama("Miss Fuzz", "Llama", "Morning")
 
-hector = Donkey("Hector", "Handsome")
+hector = Donkey("Hector", "Donkey", "Midday")
 
-ilana = Sheep("Ilana", "Talkative")
+ilana = Sheep("Ilana", "Sheep", "Midday")
 
-percy = Alpaca("Percy", "Merciful")
+percy = Alpaca("Percy", "Alpaca", "Afternoon")
 
-betty = Cow("Betty", "Butt-kisser")
+betty = Cow("Betty", "Cow", "Afternoon")
 
-dirk = Mallard("Dirk", "Jerk")
+dirk = Mallard("Dirk", "Mallard")
 
-joyce = Goldfish("Joyce", "Wise")
+joyce = Goldfish("Joyce", "Goldfish")
 
-spencer = Catfish("Spencer", "Thrifty")
+spencer = Catfish("Spencer", "Catfish")
 
-yvonne = Swan("Yvonne", "Sensual")
+yvonne = Swan("Yvonne", "Swan")
 
-joy = Koi("Joy", "Stupid")
+joy = Koi("Joy", "Koi")
 
-colin = Copperhead("Colin", "Creative")
+colin = Copperhead("Colin", "Copperhead")
 
-rachel = RatSnake("Rachel", "Untrustworthy")
+rachel = RatSnake("Rachel", "Rat Snake")
 
-sally = Salamander("Sally", "Salty")
+sally = Salamander("Sally", "Salamander")
 
-buford = Bullfrog("Buford", "Big Hearted")
+buford = Bullfrog("Buford", "Bullfrog")
 
-nell = Newt("Nell", "Nervous")
+nell = Newt("Nell", "Newt")
 
-print(colin.species)
+print(f'{hector.name} the {hector.species} is available to pet during the {hector.shift} shift.')
